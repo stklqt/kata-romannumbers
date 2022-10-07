e491888f7963325a3d1cdfdbbcd7510077f59d42
Steffen Kleeberg <steffen.kleeberg@soprasteria.com>
2022-10-07 22:53:44 +0200

    renaming and dependency updates

--- src/main/java/com/example/romannumber/RomanNumber.java
+++ src/main/java/com/example/romannumber/RomanNumber.java
@@ -1,13 +1,13 @@
-package com.example.romannumber.demo;
+package com.example.romannumber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class RomanNumber {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(RomanNumber.class, args);
     }
 
 }
--- src/test/java/com/example/romannumber/RomanNumberTests.java
+++ src/test/java/com/example/romannumber/RomanNumberTests.java
@@ -1,10 +1,10 @@
-package com.example.romannumber.demo;
+package com.example.romannumber;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class RomanNumberTests {
 
     @Test
     void contextLoads() {
