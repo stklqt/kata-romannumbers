e1954edf0e7254797dd0dedd0c5ae9f3ebac56bb
Steffen Kleeberg <steffen.kleeberg@soprasteria.com>
2022-10-07 23:16:53 +0200

    add solution without springboot test

--- src/main/java/com/example/romannumber/RomanNumber.java
+++ src/main/java/com/example/romannumber/RomanNumber.java
@@ -10,4 +10,55 @@ public class RomanNumber {
         SpringApplication.run(RomanNumber.class, args);
     }
 
+    public String convertArabicToRomanNumber(int givenNumber) {
+        String result = "";
+
+        int thousands = givenNumber / 1000;
+        result += times(thousands, "M");
+
+        int hundreds = givenNumber / 100 % 10;
+        result += times(hundreds, "C", "D", "M");
+
+        int tens = givenNumber / 10 % 10;
+        result += times(tens, "X", "L", "C");
+
+        int ones = givenNumber % 10;
+        result += times(ones, "I", "V", "X");
+
+
+        return result;
+    }
+
+    // ------------------------------- Helper Methods --------------------------------------
+    private String times(int number, String onesChar, String fivesChar, String tensChar) {
+        switch (number) {
+            case 0:
+                return "";
+            case 1:
+            case 2:
+            case 3:
+                return times(number, onesChar);
+            case 4:
+                return onesChar + fivesChar;
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+                return fivesChar + times(number - 5, onesChar);
+            case 9:
+                return onesChar + tensChar;
+            default:
+                throw new IllegalArgumentException("Only single digits allowed - not " + number);
+        }
+    }
+
+    private String times(int number, String character) {
+        String result = "";
+        for (int i = 0; i < number; i++) {
+            result += character;
+        }
+        return result;
+    }
+
+
 }
--- src/test/java/com/example/romannumber/RomanNumberTests.java
+++ src/test/java/com/example/romannumber/RomanNumberTests.java
@@ -1,13 +1,62 @@
 package com.example.romannumber;
 
+import com.tngtech.junit.dataprovider.DataProvider;
+import com.tngtech.junit.dataprovider.UseDataProvider;
+import com.tngtech.junit.dataprovider.UseDataProviderExtension;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+//@SpringBootTest
+@ExtendWith(UseDataProviderExtension.class)
 class RomanNumberTests {
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void should_return_first_number() {
+        // given
+
+        // then
+        RomanNumber number = new RomanNumber();
+        String convertedNumber = number.convertArabicToRomanNumber(1);
+
+        // when
+        assertThat(convertedNumber).isEqualTo("I");
+    }
+
+
+    @DataProvider
+    static Object[][] dataProviderRomanNumbers() {
+        return new Object[][]{
+                {"I", 1},
+                {"II", 2},
+                {"III", 3},
+                {"IV", 4},
+                {"V", 5},
+                {"VI", 6},
+                {"VIII", 8},
+                {"IX", 9},
+                {"X", 10},
+                {"XI", 11},
+                {"XIV", 14},
+                {"CCLVI", 256}
+        };
+    }
+
+    @TestTemplate
+    @UseDataProvider("dataProviderRomanNumbers")
+    void shouldTestSomeRomanNumbers(String expectedRomanNumber, int givenArabicNumber) {
+        System.out.println(expectedRomanNumber + "    " + givenArabicNumber);
+        RomanNumber romanNumber = new RomanNumber();
+
+        assertThat(romanNumber.convertArabicToRomanNumber(givenArabicNumber)).isEqualTo(expectedRomanNumber);
+    }
+
+//    https://github.com/froderik/roman_numeral_katas/blob/master/java/RomanNumber.java
+//    http://www.novaroma.org/via_romana/numbers.html
 }
